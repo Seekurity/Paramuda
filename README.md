aaead33feab4cd8d12f162fac97a262cb36a2278
Seif Elsallamy <11223632+SeifElsallamy@users.noreply.github.com>
2019-11-05 14:19:37 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,41 @@
+# paramuda 1.0 alpha
+Paramuda is a python tool designed to enumerate hidden parameters on a target URL through a wordlist. It is designed to scan for URL by counting the existence of the payload in the response body. 
+
+### easy
+```
+$ paramuda.py -u https://example.com/
+```
+![pr1](https://user-images.githubusercontent.com/11223632/68149146-01881580-ff46-11e9-87e4-9e96fb79c704.png)
+![pr2](https://user-images.githubusercontent.com/11223632/68148717-334cac80-ff45-11e9-9c5f-8b2bebbcbb7b.png)
+
+### paramuda arguments 
+```
+$ paramuda.py -h
+usage: paramuda [-h] [-v] (-u URL | -r REQUEST) [-w WORDLIST | -b {1,2,3}]
+                [-n NPARAMSPERREQ] [-t THREADS] [-p PAYLOAD]
+
+___________________________________________
+paramuda url params scan
+paramuda v.1.0 alpha
+Author: Seif Elsallamy
+Github: https://github.com/seifelsallamy/paramuda
+___________________________________________
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -v, --version         show program's version number and exit
+  -u URL, --url URL     target URL
+  -r REQUEST, --request REQUEST
+                        specific path to raw request file
+  -w WORDLIST, --wordlist WORDLIST
+                        specific path to wordlist file
+  -b {1,2,3}, --bruteforce {1,2,3}
+                        bruteforcing params level from 1 to 3 default = 2
+  -n NPARAMSPERREQ, --nparamsperreq NPARAMSPERREQ
+                        Number of parameters per request default=50
+  -t THREADS, --threads THREADS
+                        Number of threads default=5
+  -p PAYLOAD, --payload PAYLOAD
+                        Payload for testing default=qqq000
+
+```
---
+++ paramuda.py
@@ -0,0 +1,351 @@
+import threading
+import time
+import requests
+import argparse
+
+__author__='Seif Elsallamy'
+
+__version__='1.0 alpha'
+
+__url__='https://github.com/seifelsallamy/paramuda'
+
+
+__description__='''\
+___________________________________________
+paramuda url params scan
+paramuda v.'''+__version__+'''
+Author: '''+__author__+'''
+Github: '''+__url__+'''
+___________________________________________
+'''
+
+
+def print_banner():
+    print """ ____    ____  ____    ____  ___  ___  __ __  ___     ____ 
+|    \  /    T|    \  /    T|   T   T|  T  T|   \   /    T
+|  o  )Y  o  ||  D  )Y  o  || _   _ ||  |  ||    \ Y  o  |
+|   _/ |     ||    / |     ||  \_/  ||  |  ||  D  Y|     |
+|  |   |  _  ||    \ |  _  ||   |   ||  :  ||     ||  _  |
+|  |   |  |  ||  .  Y|  |  ||   |   |l     ||     ||  |  |
+l__j   l__j__jl__j\_jl__j__jl___j___j \__,_jl_____jl__j__j
+                                                          """+__version__+""""""
+
+
+inputs = []
+dynamic = []
+def reqs(req, test):
+    global debug
+    global dynamic
+    global nn
+    global signature
+    payload = "a"
+    req = req.replace("$inject$",payload)
+    req = req.split("\n")
+    raw1 = req[0].split(" ")
+    method = raw1[0]
+    path = raw1[1]
+    headers = {}
+    body = ""
+    n = 0
+    for i in req[1:]:
+        
+        if i == "":
+            n = 1
+        elif n==0:
+            p = i.split(": ")
+            if p[0].lower() == "host":
+                host = p[1].replace(" ","")
+            headers[p[0]]= p[1]
+        else:
+            body = body + i + "\n"
+    if method.lower() == "post":
+        if debug:
+            r = requests.post(path, data=body, headers=headers, proxies={"https":"127.0.0.1:8888"}, verify=False)
+        else:
+            r = requests.post(path, data=body, headers=headers)
+        m = r.text
+    elif method.lower() == "get":
+        if debug:
+            r = requests.get(path, headers=headers, proxies={"https":"127.0.0.1:8888"}, verify=False) #debug
+        else:
+            r = requests.get(path, headers=headers)
+        m = r.text
+        if test:
+            return m
+        if signature_check(m, nn, signature):
+            dynamic.append(path)
+            print "[*] Detected a dynamic param on " + path
+            
+
+    else:
+        m = "method not supported"
+    #print m
+    
+
+
+def X_Threading(nThreads, function): #X_Threading(15, printxy, [["x","y"]["hello","hey"]] )
+    global inputs
+    ready=True
+    nThreads = nThreads + threading.activeCount() # nthreads bigger than the number of already active by nthreads
+    while ready:
+        
+        if threading.activeCount() > nThreads: # if active threads bigger than nthreads stop for 0.1 second
+            time.sleep(0.1)
+            continue
+        if len(inputs) % 50 == 0:
+            print "[*] " + str(len(inputs)) + " requests left"
+        t = threading.Thread(target=function, args=(inputs.pop(0))) # send threads
+        t.start()
+        if len(inputs) < 1:
+            time.sleep(3)
+            if len(inputs) < 1:
+                ready = False
+
+    return 0
+lock = False
+def signature_check(output, n, signature):
+    #print output.count(signature) 
+    global lock
+    if lock == True:
+        time.sleep(0.1)
+    lock = True
+    c = output.count(signature)
+    if c != n:
+        #print "[*] count(payload)=" + str(c) 
+        lock = False
+        return True
+    else:
+        lock = False
+        return False
+    
+def urls_generator(url, params, n, payload):
+    if url[:4] != "http":
+        rr = url
+        url = url.split(" ")[1]
+        r = rr.replace(url,"$URL$")
+    else:
+        r = """GET $URL$ HTTP/1.1"""
+    urls = []
+    if "?" in url:
+        curl=url+"&"
+    else:
+        curl=url+"?"
+    for i in range(len(params)):
+        param = params[i]
+        if i!=0 and i % n == 0:
+            curl = curl[:-1]
+            urls.append([r.replace("$URL$",curl), 0])
+            if "?" in url:
+                curl=url + "&"
+            else:
+                curl=url + "?"
+        curl = curl + param + "=" + payload + "&"
+        
+    if n != 1:
+        lost=n-i%n
+        p=""
+        for c in range(lost-1):
+            p = p + "&0q"+ str(c) + "=" + payload
+        curl = curl + p
+        urls.append([r.replace("$URL$",curl), 0])
+
+    return urls
+
+
+def test_n(url, n, payload):
+
+    if url[:8].lower() == "get http":
+        rr = url
+        url = url.split(" ")[1]
+        r = rr.replace(url,"$URL$")
+    elif url[:4] == "http":
+        r = """GET $URL$ HTTP/1.1"""
+    elif url[:9].lower() == "post http":
+        print "POST method currently isn't supported"
+        exit(1)
+    else:
+        print "Invalid URL or raw request file"
+        print "\nExample for a valid URL"
+        print "https://example.com/xx/xx"
+        print "\nExample for a valid request "
+        print """```\nGET http://example.com/ HTTP/1.1\nXXX: YYY\n...\n\n```"""
+        exit(1)
+        
+    p = ""
+    for c in range(n):
+        p = p + "&0q"+ str(c) + "=" + payload
+    if "?" in url:
+        nurl = url + "&" + p
+    else:
+        nurl = url + "?" + p
+    req=r.replace("$URL$",nurl)
+    r=reqs(req, 1)
+    return r.count(payload)
+    
+
+#Program start
+
+
+def go(url, payload, n, tn, params):
+
+    global inputs
+    global dynamic
+    global nn
+    global signature
+    #Calculating normal n
+    nn = test_n(url, n, payload)
+    print "[*] Setting url/req to " + url
+    print "[*] Setting params per req to " + str(n)
+    print "[*] Setting payload to " + payload
+    print "[*] Setting Threads to " + str(tn)
+    print "[*] Calculating normal n"
+    print "[*] Normal n = " + str(nn)
+    signature = payload
+    #Generating requests
+    print "[*] Generating requests"
+    rs = urls_generator(url, params, n, payload)
+    print "[*] " + str(len(rs)) + " Requests"
+    print "[*] Start sending threads"
+
+
+
+    inputs = rs    
+    X_Threading(tn, reqs)
+
+
+
+    #phase 2
+    print "[*] Finished"
+
+    #New params
+    time.sleep(5)
+    if not dynamic:
+        return 0
+    print "[*] Start detecting exact dynamic parameters"
+    nparams=[]
+    for r in dynamic:
+        for p in r.split("&"):
+            nparams.append(p.split("=")[0])
+        
+    #Program start
+
+    #Setting vars
+   
+    params = nparams
+    n = 1
+    #payload = "qqq000"
+    #tn = 5 #threads
+
+    #Calculating normal n
+    nn = test_n(url, n, payload)
+    print "[*] Setting url to " + url
+    print "[*] Setting params per req to " + str(n)
+    print "[*] Setting payload to " + payload
+    print "[*] Setting Threads to " + str(tn)
+    print "[*] Calculating normal n"
+    print "[*] Normal n = " + str(nn)
+    signature = payload
+    #Generating requests
+    print "[*] Generating requests"
+    rs = urls_generator(url, params, n, payload)
+    print "[*] " + str(len(rs)) + " Requests"
+    print "[*] Start sending threads"
+
+
+
+    inputs = rs    
+    X_Threading(tn, reqs)
+
+
+
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+		version=__version__,
+		formatter_class=argparse.RawTextHelpFormatter,
+		prog='paramuda',
+		description=__description__)
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-u', '--url',type=str, help='target URL')
+    group.add_argument('-r', '--request',type=str, help='specific path to raw request file')
+
+    group2 = parser.add_mutually_exclusive_group(required=False)
+    group2.add_argument('-w', '--wordlist',type=str, help='specific path to wordlist file', required=False)
+    group2.add_argument('-b', '--bruteforce',type=int, choices=[1,2,3], help='bruteforcing params level from 1 to 3 default = 2', required=False, default=2)
+
+    parser.add_argument('-n', '--nparamsperreq', type=int, help='Number of parameters per request default=50', required=False, default=50)
+    parser.add_argument('-t', '--threads', type=int , help='Number of threads default=5', required=False, default=5)
+    parser.add_argument('-p', '--payload' , type=str, help='Payload for testing default=qqq000', required=False, default="qqq000")
+    args = parser.parse_args()
+	
+    url = args.url
+    request = args.request
+    wordlist = args.wordlist
+    bruteforce=args.bruteforce
+    n = args.nparamsperreq
+    tn = args.threads
+    payload = args.payload
+
+    print_banner()
+
+    if request is not None:
+        try:
+            with open(request,"r") as f:
+                url = f.read()
+        except:
+            print 'The file ' + request + ' could not be opened.'
+            return 1
+    if wordlist is not None:
+        try:
+            with open(wordlist,"r") as f:
+                wordlist = f.read().split("\n")
+                wordlist = filter(None, wordlist)
+        except:
+            print 'The file ' + wordlist + ' could not be opened.'
+            return 1
+        
+
+    else :
+        lv_1 = []
+        lv_2 = []
+        lv_3 = []
+        lv_4 = []
+
+        an = "_abcdefghijklmnopqrstuvwxyz1234567890"
+        
+        for c in an:
+            lv_1.append(c)
+            for cc in an:
+                lv_2.append(c+cc)
+                for ccc in an:
+                    lv_3.append(c+cc+ccc)
+                    for cccc in an:
+                        lv_4.append(c+cc+ccc+cccc)
+        if bruteforce == 1:
+            wordlist = lv_1 + lv_2
+        elif  bruteforce == 2:
+            wordlist = lv_1 + lv_2 + lv_3
+        elif bruteforce == 3:
+            wordlist = lv_1 + lv_2 + lv_3 + lv_4
+        else:
+            print "invalid bruteforce value"
+            exit(1)
+        
+    
+    
+
+    go(url, payload, n, tn, wordlist)
+
+debug = 0
+main()
+
+
+
+
+
+
+
+
+
